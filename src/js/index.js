13e5317b7095c00b031b302877eb76b735cac4b9
SamuelRicardos <samuelric4rdo@gmail.com>
2025-01-03 00:05:58 -0300

    introducao da barra de menu - responsividade

--- src/js/index.js
+++ src/js/index.js
@@ -20,3 +20,33 @@ function mostrarMaisProjetos() {
         projetoInativo.classList.add('ativo');
     });
 }
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.menu');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if( top >= offset && top < offset + height) {
+           navLinks.forEach(links => {
+            links.classList.remove('active');
+            let activeLink = document.querySelector('header nav a[href*=' + id + ']');
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
+            
+           }) 
+        }
+    })
+}
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+})
